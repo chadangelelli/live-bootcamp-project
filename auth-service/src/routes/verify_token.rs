e948cf69403c6b31db0918ba0eb6fbdd111d52aa
Chad Angelelli <chad.angelelli@gmail.com>
2025-08-10 15:50:04 -0600

    auth-service: fix verify_token bug
    - was not letting app protected route pass

--- auth-service/src/routes/verify_token.rs
+++ auth-service/src/routes/verify_token.rs
@@ -1,29 +1,16 @@
-use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
-use axum_extra::extract::CookieJar;
+use axum::{extract::State, http::StatusCode, Json};
 use serde::Deserialize;
 
-use crate::{
-    app_state::AppState, domain::AuthApiError, utils::auth::validate_token,
-    utils::constants::JWT_COOKIE_NAME,
-};
+use crate::{app_state::AppState, domain::AuthApiError, utils::auth::validate_token};
 
 pub async fn verify_token(
     State(state): State<AppState>,
-    jar: CookieJar,
     Json(request): Json<VerifyTokenRequest>,
-) -> Result<(CookieJar, impl IntoResponse), AuthApiError> {
-    validate_token(&request.token, state.banned_token_store.clone())
-        .await
-        .map_err(|_| AuthApiError::InvalidToken)?;
-
-    let cookie = jar.get(JWT_COOKIE_NAME).ok_or(AuthApiError::MissingToken)?;
-    let existing_token = cookie.value().to_owned();
-
-    if &request.token != existing_token.as_str() {
-        return Err(AuthApiError::Unauthorized);
+) -> Result<StatusCode, AuthApiError> {
+    match validate_token(&request.token, state.banned_token_store.clone()).await {
+        Ok(_) => Ok(StatusCode::OK),
+        Err(_) => Err(AuthApiError::InvalidToken),
     }
-
-    Ok((jar, StatusCode::OK.into_response()))
 }
 
 #[derive(Deserialize)]
